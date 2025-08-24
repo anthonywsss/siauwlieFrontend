a4ab6251ee24a068eb7e0b3d67ea2f9b6ef3a6fc
You2wo <ashton.therestiawan@gmail.com>
2025-08-24 16:38:06 +0700

    changed page routing, new comp table

--- src/components/Layouts/sidebar/data/index.ts
+++ src/components/Layouts/sidebar/data/index.ts
@@ -27,7 +27,7 @@ export const NAV_DATA = [
       },
       {
         title: "Submit Movement",
-        url: "/SubmitMovement",
+        url: "/submit-movement",
         icon: Icons.ChartIcon,
         items: [],
       },

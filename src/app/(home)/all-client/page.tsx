1c17156500e67856dffb9e6410c4677916275146
You2wo <ashton.therestiawan@gmail.com>
2025-08-28 22:55:40 +0700

    okd

--- src/app/(home)/all-client/page.tsx
+++ src/app/(home)/all-client/page.tsx
@@ -1,33 +1,15 @@
-<<<<<<< HEAD
 import AllItemsClient from "@/components/Tables/all-client";
-=======
-import SearchBar from "@/components/Search/SearchBar";
-import { AllItemsTable } from "@/components/Tables/allitems";
-import AllClient from "@/components/Tables/allclient.client";
->>>>>>> b0e563964f0ccd88fcb34c90a5a0a89885e57706
 
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-<<<<<<< HEAD
   title: "All Asset",
-=======
-  title: "All Client",
->>>>>>> b0e563964f0ccd88fcb34c90a5a0a89885e57706
 };
 
 export default function Home() {
   return (
     <div className="space-y-6">
-<<<<<<< HEAD
       <AllItemsClient />
     </div>
   );
 }
-=======
-
-      <AllClient />
-    </div>
-  );
-}
->>>>>>> b0e563964f0ccd88fcb34c90a5a0a89885e57706

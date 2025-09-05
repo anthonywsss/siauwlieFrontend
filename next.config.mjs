5dc13b07252e91c3f0ac8a26a83512c19c288e9b
You2wo <ashton.therestiawan@gmail.com>
2025-09-05 18:12:12 +0700

    TOAST

--- next.config.mjs
+++ next.config.mjs
@@ -5,25 +5,24 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
-        port: ""
       },
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-        port: ""
       },
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
-        port: ""
       },
       {
         protocol: "https",
         hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
-        port: ""
-      }
-    ]
-  }
+      },
+    ],
+  },
+  devIndicators: {
+    position: "bottom-right", // or 'bottom-right', 'top-left', 'top-right'
+  },
 };
 
 export default nextConfig;

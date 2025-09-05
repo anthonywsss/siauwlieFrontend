0035626c31029caee1e434ea0c88f21ffc51ad41
Anne <vallerie.anne12@gmail.com>
2025-09-05 15:33:39 +0700

    remove layout login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,24 @@
+// app/layout.tsx
 import "@/css/satoshi.css";
 import "@/css/style.css";
 
 import { Sidebar } from "@/components/Layouts/sidebar";
+import { Header } from "@/components/Layouts/header";
 
 import "flatpickr/dist/flatpickr.min.css";
 import "jsvectormap/dist/jsvectormap.css";
 
-import { Header } from "@/components/Layouts/header";
 import type { Metadata } from "next";
 import NextTopLoader from "nextjs-toploader";
 import type { PropsWithChildren } from "react";
 import { Providers } from "./providers";
 import { AuthProvider } from "@/components/Auth/auth-context";
+import LayoutWrapper from "./layout-wrapper"; 
 
 export const metadata: Metadata = {
   title: {
     template: "%s | Siauw Pabrik Tahu",
-    default: "Siau Pabrik Tahu",
+    default: "Siauw Pabrik Tahu",
   },
   description: "p",
 };
@@ -28,18 +30,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
         <Providers>
           <AuthProvider>
             <NextTopLoader color="#5750F1" showSpinner={false} />
-
-            <div className="flex min-h-screen">
-              <Sidebar />
-
-              <div className="w-full bg-gray-2 dark:bg-[#020d1a]">
-                <Header />
-
-                <main className="isolate mx-auto w-full max-w-screen-2xl overflow-hidden p-4 md:p-6 2xl:p-10">
-                  {children}
-                </main>
-              </div>
-            </div>
+            <LayoutWrapper>{children}</LayoutWrapper>
           </AuthProvider>
         </Providers>
       </body>

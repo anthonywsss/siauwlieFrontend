f09d2f7c41291cf7a41b510184cb390d37b4671d
Anne <vallerie.anne12@gmail.com>
2025-09-11 18:54:14 +0700

    .

--- src/components/CreateNewAsset.tsx
+++ src/components/CreateNewAsset.tsx
@@ -471,6 +471,7 @@ const resetForm = () => {
                     onClick={() => {
                       resetForm();
                       onClose();
+                      onCreated();
                     }}
                     className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors"
                   >
